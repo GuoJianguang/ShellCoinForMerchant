40c78bde9d8ed077fdc98609207d7a2c653ac33c
GuoMr <873191027@qq.com>
2017-03-22 16:49:29 +0800

    12

--- ShellCoinForMerchant/ViewController.m
+++ ShellCoinForMerchant/ViewController.m
@@ -17,8 +17,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-    self.back
-    
+        self.view.backgroundColor
 }
 
 
